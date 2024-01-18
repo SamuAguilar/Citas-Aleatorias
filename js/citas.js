42c3e9b8da9d5df84e73df5c5c95d1744a56a381
SamuAguilar <samuaguilar93@gmail.com>
2024-01-17 21:56:48 -0300

    Citas-Aleatorias

---
+++ js/citas.js
@@ -0,0 +1,70 @@
+let citas = [
+  {
+    'autor': 'Albert Einstein',
+    'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+  },
+  {
+    'autor': 'Anónimo',
+    'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+  },
+  {
+    'autor': 'Alan Kay',
+    'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+  },
+  {
+    'autor': 'Amelia Earhart',
+    'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+  },
+  {
+    'autor': 'Aristotle',
+    'texto': 'La calidad no es un acto, es un hábito.'
+  },
+  {
+    'autor': 'Benjamin Franklin',
+    'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+  },
+  {
+    'autor': 'Charles R. Swindoll',
+    'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+  },
+  {
+    'autor': 'Jane Goodall',
+    'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+  },
+  {
+    'autor': 'John Muir',
+    'texto': 'El poder de la imaginación nos hace infinitos.'
+  },
+  {
+    'autor': 'Mark Twain',
+    'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+  },
+  {
+    'autor': 'Dolores Ibarruri',
+    'texto': 'Es mejor morir de pie que vivir arrodillado.'
+  },
+  {
+    'autor': 'Friedrich Nietzsche',
+    'texto': 'El mundo es bello, pero tiene un defecto llamado hombre.'
+  },
+  {
+    'autor': 'Friedrich Nietzsche',
+    'texto': 'Lo que no te mata, te hace más fuerte.'
+  },
+  {
+    'autor': 'Marco Tulio Cicerón',
+    'texto': 'De humanos es errar y de necios permanecer en el error.'
+  },
+  {
+    'autor': 'John Fitzgerald Kennedy',
+    'texto': 'El éxito tiene muchos padres, pero el fracaso es huérfano.'
+  },
+  {
+    'autor': 'Goethe',
+    'texto': 'El único hombre que no se equivoca es el que nunca hace nada.'
+  },
+  {
+    'autor': 'Maquiavelo',
+    'texto': 'Vale más hacer y arrepentirse, que no hacer y arrepentirse.'
+  }
+];
